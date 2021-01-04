f2c4ebe45fac504cbd4f709d0933ff5c314caca7
LovelyHaRa <43318609+LovelyHaRa@users.noreply.github.com>
2021-01-04 11:59:56 +0900

    feat: add testing code to index(sample) (#2)
    * feat: add package for mocking css
    
    * feat: add testing code to index

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
   setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+  moduleNameMapper: {
+    "\\.(css|less)$": "identity-obj-proxy",
+  },
 };
---
+++ pages/index.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { render } from "@testing-library/react";
+
+import Home from ".";
+
+describe("<Home />", () => {
+  it("should be render", () => {
+    const { getByText } = render(<Home />);
+    expect(getByText("Welcome to")).toBeInTheDocument();
+    expect(getByText("Next.js!")).toBeInTheDocument();
+  });
+});
